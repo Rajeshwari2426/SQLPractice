05ce0a8f61d088bd6f897637d4f980d28b10a306
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-05-07 11:46:30 +0530

    [Raji] adding views

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -21,3 +21,32 @@ SELECT  c.id candidate_id,c.fullname candidate_name,e.id employee_id,e.fullname 
 SELECT  c.id candidate_id,c.fullname candidate_name,e.id employee_id,e.fullname employee_name FROM  hr.candidates c right JOIN hr.employees e  ON e.fullname = c.fullname;
 INSERT INTO HR.Employees(fullname)VALUES('Draco Malfoy '),('Lord Voldmort'),('Bellatrix Lestrange'),('Doleres Umbridge');
 SELECT  c.id candidate_id,c.fullname candidate_name,e.id employee_id,e.fullname employee_name FROM  hr.candidates c right JOIN hr.employees e  ON e.fullname = c.fullname where c.id is null;
+----Full Joins----
+SELECT  c.id candidate_id,c.fullname candidate_name,e.id employee_id,e.fullname employee_name FROM  hr.candidates c  full JOIN hr.employees e  ON e.fullname = c.fullname;
+SELECT  c.id candidate_id,c.fullname candidate_name,e.id employee_id,e.fullname employee_name FROM  hr.candidates c  full JOIN hr.employees e  ON e.fullname = c.fullname where c.id is null or e.id is null;
+alter table HR.Employees add Basic_Pay float not null default 0.00;
+update HR.Employees set Basic_Pay=70000.00 where Id=1; 
+update HR.Employees set Basic_Pay=75000.00 where Id=2; 
+update HR.Employees set Basic_Pay=60000.00 where Id=3; 
+update HR.Employees set Basic_Pay=80000.00 where Id=4; 
+SELECT  c.id candidate_id,c.fullname candidate_name,c.e.id employee_id,e.fullname employee_name FROM  hr.candidates c  full JOIN hr.employees e  ON e.fullname = c.fullname where c.id is null or e.id is null;
+alter table HR.Candidates add Basic_Pay float not null default 0.00;
+update HR.Candidates set Basic_Pay=60000.00 where Id=1; 
+update HR.Candidates set Basic_Pay=55000.00 where Id=2; 
+update HR.Candidates set Basic_Pay=40000.00 where Id=3; 
+update HR.Candidates set Basic_Pay=30000.00 where Id=4; 
+-----Views-----------
+SELECT  c.id candidate_id,c.fullname candidate_name,e.id employee_id,e.fullname employee_name FROM  hr.candidates c Left JOIN hr.employees e  ON e.fullname = c.fullname;
+
+CREATE VIEW leftjoin_info as SELECT  c.id candidate_id,c.fullname candidate_name,e.id employee_id,e.fullname employee_name FROM  hr.candidates c left JOIN hr.employees e  ON e.fullname = c.fullname;
+select * from leftjoin_info;
+CREATE VIEW rightjoin_info as SELECT  c.id candidate_id,c.fullname candidate_name,e.id employee_id,e.fullname employee_name FROM  hr.candidates c right JOIN hr.employees e  ON e.fullname = c.fullname;
+select * from rightjoin_info;
+Drop View if exists HR.leftjoin_info;
+SELECT 
+	OBJECT_SCHEMA_NAME(v.object_id) schema_name,
+	v.name
+FROM 
+	sys.views as v;
+CREATE VIEW HR.rightjoin_info as SELECT  c.id candidate_id,c.fullname candidate_name,e.id employee_id,e.fullname employee_name FROM  hr.candidates c right JOIN hr.employees e  ON e.fullname = c.fullname;
+Drop View if exists HR.rightjoin_info;
